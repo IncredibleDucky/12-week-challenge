b222e835ecc500ea6f08c9f787e704cbeb20ab69
Matt Bailey <incredibleducky@gmail.com>
2015-10-01 16:49:24 -0700

    Initial Commit

---
+++ trick/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  trick
+//
+//  Created by Mattthew Bailey on 10/1/15.
+//  Copyright © 2015 Mattthew Bailey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
